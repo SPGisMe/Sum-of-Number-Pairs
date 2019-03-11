276cd78c089041aa27816bc5752a46a5cdce1863
Sean Gaston <46758834+SPGisMe@users.noreply.github.com>
2019-03-11 13:52:02 -0700

    Create sumofpairs.js

---
+++ sumofpairs.js
@@ -0,0 +1,14 @@
+var sum_pairs=function(ints, s){
+  const values = {}
+  
+  for (i = 0; i < ints.length; i++) {
+  //add key:value pairs for the solution
+    if (values[ints[i]] !== undefined) {
+      return [values[ints[i]], ints[i]];
+    } else {
+      let diff = s - ints[i];
+      values[diff] = ints [i];
+    }
+  }
+  return undefined;
+}
